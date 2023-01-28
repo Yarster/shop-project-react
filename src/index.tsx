49d66cd1c644753918cffa4e775542b60fa24915
Yarster <xumuk132@gmail.com>
2023-01-28 13:49:47 +0200

    content added

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+const title = React.createElement('h1', null, 'Hello world')
+console.log(title)
+
+let content = React.createElement('p', null, 'Hello world blab bald ald')
+
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
-  <React.StrictMode></React.StrictMode>
-);
+    <React.StrictMode>
+        {content}
+        {content}
+    </React.StrictMode>
+)
