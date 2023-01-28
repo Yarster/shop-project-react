5ef070d2d67703578fa8f47cb5cc5c461fbd043b
Yarster <xumuk132@gmail.com>
2023-01-28 14:04:37 +0200

    class

--- src/index.tsx
+++ src/index.tsx
@@ -6,10 +6,26 @@ console.log(title)
 
 let content = React.createElement('p', null, 'Hello world blab bald ald')
 
+// jsx
+
+const title = (
+    <h1 id="title" className="test">
+        Hello world
+    </h1>
+)
+const content = (
+    <div>
+        <p>
+            <span>Hello</span>p
+        </p>
+        <p>Hello p2</p>
+    </div>
+)
+
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
     <React.StrictMode>
-        {content}
+        {title}
         {content}
     </React.StrictMode>
 )
