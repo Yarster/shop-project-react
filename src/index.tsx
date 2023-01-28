b9c15b69c2a7abd906a7d8d9ea9380053d10657e
Yarster <xumuk132@gmail.com>
2023-01-28 13:12:10 +0200

    removed 2

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,5 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-  </React.StrictMode>
+  <React.StrictMode></React.StrictMode>
 );
