c820cabf46d5f4325eb1d8fdbca276ccdebcf939
Yarster <xumuk132@gmail.com>
2023-01-29 14:11:49 +0200

    classes with hash added

--- src/Content.tsx
+++ src/Content.tsx
@@ -1,4 +1,6 @@
-import './Content.css'
+import classes from './Content.module.css'
+
+const { content, purple, peru } = classes
 
 type ContentProps = {
     text1: string
@@ -9,7 +11,7 @@ const Content = ({ text1, text2 }: ContentProps) => {
     let data = false
     return (
         <>
-            <p className={`content ${data ? 'purple' : 'peru'}`}>{text1}</p>
+            <p className={`${content} ${data ? purple : peru}`}>{text1}</p>
             <p>{text2}</p>
         </>
     )
