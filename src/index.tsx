0faf31c1968ec9f50aaf2640849a39b60cf6229f
Yarster <xumuk132@gmail.com>
2023-01-29 14:20:15 +0200

    cleaned all

--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { Header } from './Header'
-import Content from './Content'
-import List from './List'
-
-const App = () => {
-    return (
-        <>
-            <Header title="React" year={2023} />
-            <Header title="TS" />
-            <Content text1="lorem" text2="lorem2" />
-            <List item1="App" item2="React" item3="TS" />
-        </>
-    )
-}
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
-root.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>
-)
+root.render(<React.StrictMode></React.StrictMode>)
