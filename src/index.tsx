7887d8e43bc757cf28c909258f8730ca76451355
Yarster <xumuk132@gmail.com>
2023-01-29 13:46:59 +0200

    list added

--- src/index.tsx
+++ src/index.tsx
@@ -1,26 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Header } from './Header'
+import Content from './Content'
+import List from './List'
 
-type ContentProps = {
-    text1: string
-    text2: string
-}
-
-const Content = ({ text1, text2 }: ContentProps) => {
-    return (
-        <React.Fragment>
-            <p>{text1}</p>
-            <p>{text2}</p>
-        </React.Fragment>
-    )
-}
 const App = () => {
     return (
         <>
             <Header title="React" year={2023} />
             <Header title="TS" />
             <Content text1="lorem" text2="lorem2" />
+            <List item1="App" item2="React" item3="TS" />
         </>
     )
 }
