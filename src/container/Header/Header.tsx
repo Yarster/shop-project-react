80d2bf17e267f49ca15d2beaf4d1bc0ad0258be3
Yarster <xumuk132@gmail.com>
2023-02-04 12:53:38 +0200

    Logo import

--- src/container/Header/Header.tsx
+++ src/container/Header/Header.tsx
@@ -1,11 +1,10 @@
 import AppBar from '@mui/material/AppBar'
 import Toolbar from '@mui/material/Toolbar'
-import Typography from '@mui/material/Typography'
 import Button from '@mui/material/Button'
 import IconButton from '@mui/material/IconButton'
 import MenuIcon from '@mui/icons-material/Menu'
 import Container from '@mui/material/Container'
-import logo from 'assets/logo.svg'
+import Logo from 'components/Logo/Logo'
 
 type Props = {}
 const Header = (props: Props) => {
@@ -28,13 +27,7 @@ const Header = (props: Props) => {
                         >
                             <MenuIcon />
                         </IconButton>
-                        <Typography
-                            variant="h6"
-                            component="div"
-                            sx={{ flexGrow: 1 }}
-                        >
-                            <img src={logo} />
-                        </Typography>
+                        <Logo />
                         <Button color="inherit">Login</Button>
                     </Toolbar>
                 </Container>
