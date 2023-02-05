bcb8ada4f3d04ed295edacc5c300465f24bd9327
Yarster <xumuk132@gmail.com>
2023-02-05 14:49:16 +0200

    incremet, decrement added

--- src/components/ProductsList/ProductListItem.tsx
+++ src/components/ProductsList/ProductListItem.tsx
@@ -17,7 +17,31 @@ type Props = {
     image: string
 }
 
-class ProductListItem extends Component<Props> {
+type State = {
+    count: number
+}
+
+class ProductListItem extends Component<Props, State> {
+    constructor(props: Props) {
+        super(props)
+        this.state = {
+            count: 1,
+        }
+        this.onIncrementClick = this.onIncrementClick.bind(this)
+        this.onDecrementClick = this.onDecrementClick.bind(this)
+    }
+    onIncrementClick() {
+        this.setState((prevState) => ({
+            count: prevState.count + 1,
+        }))
+    }
+
+    onDecrementClick() {
+        this.setState((prevState) => ({
+            count: prevState.count - 1,
+        }))
+    }
+
     render() {
         const { title, description, type, capacity, price, image } = this.props
         return (
@@ -32,9 +56,19 @@ class ProductListItem extends Component<Props> {
                     <div className="product-features">Capacity:{capacity}</div>
                     <div className="product-price">Price:{price}</div>
                     <div className="product-quantity">
-                        <Button variant="outlined">-</Button>
-                        <TextField size="small" value="1" />
-                        <Button variant="outlined">+</Button>
+                        <Button
+                            variant="outlined"
+                            onClick={this.onDecrementClick}
+                        >
+                            -
+                        </Button>
+                        <TextField size="small" value={this.state.count} />
+                        <Button
+                            variant="outlined"
+                            onClick={this.onIncrementClick}
+                        >
+                            +
+                        </Button>
                     </div>
                 </CardContent>
                 <CardActions className="btns-wrap">
