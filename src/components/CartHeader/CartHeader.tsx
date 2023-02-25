2d659efa363856f41a489f3651dad4028509b3c8
Yarster <xumuk132@gmail.com>
2023-02-25 21:28:02 +0200

    added total price to cartHeader

--- src/components/CartHeader/CartHeader.tsx
+++ src/components/CartHeader/CartHeader.tsx
@@ -15,12 +15,25 @@ const CartHeader = ({
 }: Props) => {
     return (
         <div>
-            {Object.keys(productsInCart).map((productID) => (
-                <div key={productID}>
-                    {productsObject[parseInt(productID)].title}:{' '}
-                    {productsInCart[parseInt(productID)]}
-                </div>
-            ))}
+            <div>
+                {Object.keys(productsInCart).map((productId) => (
+                    <div key={productId}>
+                        {productsObject[parseInt(productId)].title}:{' '}
+                        {productsInCart[parseInt(productId)]}
+                    </div>
+                ))}
+            </div>
+            <div>
+                Total:{' '}
+                {Object.keys(productsInCart).reduce(
+                    (total, productId) =>
+                        total +
+                        productsObject[parseInt(productId)].price *
+                            productsInCart[parseInt(productId)],
+                    0
+                )}
+                $
+            </div>
         </div>
     )
 }
