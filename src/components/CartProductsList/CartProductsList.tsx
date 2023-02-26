84a91eaf5933f607f62433b9db155cece284313e
Yarster <xumuk132@gmail.com>
2023-02-26 13:36:01 +0200

    added cart page image

--- src/components/CartProductsList/CartProductsList.tsx
+++ src/components/CartProductsList/CartProductsList.tsx
@@ -1,4 +1,5 @@
 import productsArray, { getProductsObject, Product } from 'utils/productsArray'
+import CartProductsListItem from './CartProductsListItem'
 
 type Props = {
     productsInCart: {
@@ -7,21 +8,24 @@ type Props = {
     productsObject?: {
         [id: number]: Product
     }
+    CartItem?: any
 }
 
 const CartProductsList = ({
     productsInCart,
     productsObject = getProductsObject(productsArray),
+    CartItem = CartProductsListItem,
 }: Props) => {
     return (
-        <div>
+        <>
             {Object.keys(productsInCart).map((productId) => (
-                <div key={productId}>
-                    {productsObject[parseInt(productId)].title}:{' '}
-                    {productsInCart[parseInt(productId)]}
-                </div>
+                <CartItem
+                    key={productId}
+                    product={productsObject[parseInt(productId)]}
+                    productCount={productsInCart[parseInt(productId)]}
+                />
             ))}
-        </div>
+        </>
     )
 }
 export default CartProductsList
--- src/pages/CartPage/CartPage.tsx
+++ src/pages/CartPage/CartPage.tsx
@@ -1,5 +1,6 @@
-import { Typography } from '@mui/material'
+import { Grid, Typography } from '@mui/material'
 import CartProductsList from 'components/CartProductsList/CartProductsList'
+import CartProductsListItemExtended from 'components/CartProductsList/CartProductsListItemExtended'
 import CartTotal from 'components/CartTotal/CartTotal'
 
 type Props = {
@@ -14,7 +15,12 @@ const CartPage = ({ productsInCart }: Props) => {
             <Typography component="h1" variant="h4">
                 Cart
             </Typography>
-            <CartProductsList productsInCart={productsInCart} />
+            <Grid container spacing={4} sx={{ margin: 0 }}>
+                <CartProductsList
+                    productsInCart={productsInCart}
+                    CartItem={CartProductsListItemExtended}
+                />
+            </Grid>
             <CartTotal productsInCart={productsInCart} />
         </div>
     )
